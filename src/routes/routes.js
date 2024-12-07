4e08c7cedf81f405fc0f5a92ee6f03cf4c8d428a
a23893 <a23893@alunos.ipca.pt>
2024-12-07 12:29:36 +0000

    auth fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import userInEventController from "../controllers/userInEventController.js";
 import feedbackController from "../controllers/feedbackController.js";
 import {verifyToken }from "../middlewares/authMiddleware.js";
+import { verifyAdmin } from "../middlewares/adminMiddleware.js";
+import {verifyAdvertiser} from "../middlewares/advertiserMiddleware.js";
 
 const router = Router();
 
 // UserInEvents Routes
-router.get('/tickets', userInEventController.getAllUserInEvents);
+router.get('/tickets', verifyToken, verifyAdmin, userInEventController.getAllUserInEvents);
 router.get('/tickets/my', verifyToken, userInEventController.getUserTickets);
 router.get('/tickets/:id', userInEventController.getUserInEventById);
 router.get('/tickets/event/:eventId', userInEventController.getTicketsByEvent);
@@ -19,7 +21,7 @@ router.put('/tickets/qrcode/read/:ticketId', userInEventController.updateUserPar
 //rota do ticket qr code
 
 // Feedback Routes
-router.get('/feedbacks', feedbackController.getAllFeedbacks);
+router.get('/feedbacks', verifyToken, verifyAdmin, feedbackController.getAllFeedbacks);
 router.get('/feedbacks/:id', feedbackController.getFeedbackById);
 router.post('/feedbacks/:ticketID', verifyToken, feedbackController.createFeedback);
 router.put('/feedbacks/:id', feedbackController.updateFeedback);
